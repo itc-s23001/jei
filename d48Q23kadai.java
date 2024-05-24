958e4f11895bae0a5be4f1722c7b021073524961
itc-s23001 <s23001@std.it-college.ac.jp>
2024-05-24 11:32:54 +0900

    第４８回問２３

---
+++ d48Q23kadai.java
@@ -0,0 +1,19 @@
+//s23001
+//System,out.println(i);の位置を変更した
+
+
+class d48Q23 {
+  public static void main(String[] args) {
+   int i = 1;
+   for (;;) {
+     i = i + 3;
+     if (i < 10) {
+       continue;
+     } else {
+       break;
+       }
+     }
+     System.out.println(i);
+  }
+}
+
