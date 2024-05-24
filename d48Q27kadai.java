a9fe6ca1a5859933bbdc4883eaed00c8743001a9
itc-s23001 <s23001@std.it-college.ac.jp>
2024-05-24 12:04:20 +0900

    第４８回問２７

---
+++ d48Q27kadai.java
@@ -0,0 +1,13 @@
+//s23001
+//iの値を毎回1ずつ増やしてループを終了させる
+
+public class d48Q27kadai {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 6) {
+            System.out.print(i);
+            i++; 
+        }
+    }
+}
+
