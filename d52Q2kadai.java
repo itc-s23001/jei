fc79aedc03f2a0cec2c8197cbc410e18c098a2bb
itc-23001 <s23001@std.itcollege.ac.jp>
2024-08-02 01:55:14 +0000

    d52Q2kadai

---
+++ d52Q2kadai.java
@@ -0,0 +1,23 @@
+//s23001
+//
+
+class Temper {
+    private double temperature;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+}
+
+public class d52Q2kadai {
+    public static void main(String[] args) {
+        Temper t = new Temper();
+        t.setTemperature(17.2);
+        System.out.println("Temperature: " + t.getTemperature());
+    }
+}
+
